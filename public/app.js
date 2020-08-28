488b646f255a501359f1c5e8c03e5451c1f5dc0b
Eric Park <epark0995@gmail.com>
2020-08-27 20:20:47 -0700

    final changes

--- public/app.js
+++ public/app.js
@@ -1,50 +1,50 @@
 document.getElementById('add').addEventListener('click', event => {
-    event.preventDefault()
-  
-    axios.post('api/burgers', {
-      burger_name: document.getElementById('burger').value,
-      devoured: false
-    })
-      .then(({ data }) => {
-        let burgerElem = document.createElement('li')
-        burgerElem.id = data.id
-        burgerElem.className = 'list-group-item list-group-item-info'
-        burgerElem.dataset.name = document.getElementById('burger').value
-        burgerElem.innerHTML = `
+  event.preventDefault()
+
+  axios.post('api/burgers', {
+    burger_name: document.getElementById('burger').value,
+    devoured: false
+  })
+    .then(({ data }) => {
+      let burgerElem = document.createElement('li')
+      burgerElem.id = data.id
+      burgerElem.className = 'list-group-item list-group-item-info'
+      burgerElem.dataset.name = document.getElementById('burger').value
+      burgerElem.innerHTML = `
             ${document.getElementById('burger').value}
           <button class="btn btn-success devour">Devour It!</button>
         `
 
-        document.getElementById('notDevoured').append(burgerElem)
+      document.getElementById('notDevoured').append(burgerElem)
 
-        document.getElementById('burger').value = ''
-      })
-      .catch(err => console.log(err))
-  })
-  
+      document.getElementById('burger').value = ''
+    })
+    .catch(err => console.log(err))
+})
 
-  document.addEventListener('click', event => {
-    event.preventDefault()
-    if (event.target.classList.contains('devour')) {
-      axios.put(`api/burgers/${event.target.parentNode.id}`, {
-        devoured: true
-      })
-      .then(({data}) => {
-        console.log(({data}))
-  
+
+document.addEventListener('click', event => {
+  event.preventDefault()
+  if (event.target.classList.contains('devour')) {
+    axios.put(`api/burgers/${event.target.parentNode.id}`, {
+      devoured: true
+    })
+      .then(({ data }) => {
+        console.log(({ data }))
+
         let burgerElem = document.createElement('li')
         burgerElem.id = data.id
         burgerElem.className = 'list-group-item list-group-item-danger'
-        burgerElem.innerHTML = `
-            ${event.target.parentNode.dataset.name}
-        `
- 
-        document.getElementById('devoured').append(burgerElem)
+        burgerElem.innerHTML = 
+            `${event.target.parentNode.dataset.name}`
         
 
+        document.getElementById('devoured').append(burgerElem)
+
+
         event.target.parentNode.remove()
-        
+
       })
       .catch(err => console.log(err))
-    }
-  })
+  }
+})
