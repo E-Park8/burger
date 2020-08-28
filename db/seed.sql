9679ec3fb62731cc8227ecbd9336fede07ba4691
Eric Park <epark0995@gmail.com>
2020-08-27 20:44:49 -0700

    final changes

--- db/seed.sql
+++ db/seed.sql
@@ -1,8 +1,8 @@
-USE burger_db;
+USE ne1w6jye515g3cal;
 
 INSERT INTO burgers(burger_name, devoured)
 VALUES ('Double Cheeseburger', true),
        ('Blue Cheese Burger', false),
-       ('Hangover Burger', false).
+       ('Hangover Burger', false),
        ('Protein Style', true),
        ('Heart Attack', false)
