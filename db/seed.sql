6443d5669e11547ad7bc3703c17bf9940b4e3f12
Eric Park <epark0995@gmail.com>
2020-08-27 19:52:14 -0700

    functional

---
+++ db/seed.sql
@@ -0,0 +1,8 @@
+USE burger_db;
+
+INSERT INTO burgers(burger_name, devoured)
+VALUES ('Double Cheeseburger', true),
+       ('Blue Cheese Burger', false),
+       ('Hangover Burger', false).
+       ('Protein Style', true),
+       ('Heart Attack', false)
